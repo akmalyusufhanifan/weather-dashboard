ecbe8703682af903b986423a62e892cbcd9e769c
akmalyusufhanifan <akmalyusufhanifan@gmail.com>
2026-04-13 12:46:17 +0700

    feat: initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,73 +1,42 @@
-"use client";
-
-import { useState } from "react";
-import axios from "axios";
-import { type WeatherData } from "@/types/weather-data";
+import TodayWeatherCard from "@/components/today-weather-card";
+import NextDayWeatherCard from "@/components/next-day-weather-card";
 
 export default function Home() {
-  const [data, setData] = useState<WeatherData | null>(null);
-  const [location, setLocation] = useState("");
-
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=e985c524985e9c73325052ed31879977`;
-
-  const searchLocation = (event: any) => {
-    if (event.key === "Enter") {
-      axios.get(url).then((res) => {
-        setData(res.data);
-        console.log(res.data);
-      });
-      setLocation("");
-    }
-  };
-
   return (
-    <div className="h-screen bg-[url('/background.webp')] bg-cover bg-center relative py-8 px-8 overflow-hidden">
-      <div className="absolute inset-0 bg-black/30"></div>
-
-      <div className="h-full relative z-10 flex flex-col justify-between items-center">
-        {/* search bar */}
-        <input
-          value={location}
-          onChange={(event) => setLocation(event.target.value)}
-          onKeyDown={searchLocation}
-          placeholder="Enter Location"
-          type="text"
-          className="bg-white/20 w-[250px] h-12 rounded-full border border-white pl-4 text-lg text-white"
-        />
-
-        {/* main content */}
-        <div className="w-full flex flex-col items-start gap-y-2">
-          <h2 className="text-xl tracking-wide">{data?.name}</h2>
-          <div className="w-full flex justify-between items-center">
-            <h1 className="text-8xl">{data?.main.temp.toFixed()}°C</h1>
-            <h2 className="text-xl rotate-270">{data?.weather[0].main}</h2>
+    <div className="flex flex-col items-center">
+      {/* row 1 */}
+      <div>
+        {/* next 7 days */}
+        <div className="carousel carousel-center w-[257px] h-[226px] gap-x-8">
+          <div className="carousel-item">
+            <TodayWeatherCard />
+          </div>
+          <div className="carousel-item">
+            <NextDayWeatherCard />
+          </div>
+          <div className="carousel-item">
+            <NextDayWeatherCard />
+          </div>
+          <div className="carousel-item">
+            <NextDayWeatherCard />
+          </div>
+          <div className="carousel-item">
+            <NextDayWeatherCard />
+          </div>
+          <div className="carousel-item">
+            <NextDayWeatherCard />
+          </div>
+          <div className="carousel-item">
+            <NextDayWeatherCard />
           </div>
         </div>
 
-        {/* description */}
-        <div className="bg-white/15 w-full h-22 rounded-3xl shadow-xs flex justify-between items-center px-6">
-          <div className="flex flex-col items-center">
-            <h2 className="text-2xl font-bold tracking-wide">
-              {data?.main.feels_like.toFixed()}°C
-            </h2>
-            <h3 className="text-xl">Feels Like</h3>
-          </div>
-
-          <div className="flex flex-col items-center">
-            <h2 className="text-2xl font-bold tracking-wide">
-              {data?.main.humidity}%
-            </h2>
-            <h3 className="text-xl">Humidity</h3>
-          </div>
-
-          <div className="flex flex-col items-center">
-            <h2 className="text-2xl font-bold tracking-wide">
-              {data?.wind.speed.toFixed()}MPH
-            </h2>
-            <h3 className="text-xl">Wind Speed</h3>
-          </div>
-        </div>
+        {/* chance of rain */}
+        <div></div>
       </div>
+
+      {/* row 2 */}
+      <div></div>
     </div>
   );
 }
---
+++ src/components/next-day-weather-card.tsx
@@ -0,0 +1,11 @@
+export default function NextDayWeatherCard() {
+  return (
+    <div className="w-[96px] h-[226px] bg-[#1B1B1D] rounded-3xl flex flex-col justify-between items-center p-4">
+      <h2 className="text-md border-b-2 border-gray-700 w-[96px] text-center">
+        SAT
+      </h2>
+      <h2 className="text-3xl">🌦️</h2>
+      <h2 className="text-4xl">10°</h2>
+    </div>
+  );
+}
