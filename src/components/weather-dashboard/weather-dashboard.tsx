f6a7cc05317d3c00c00a1e5f0b1311ce10265de7
akmalyusufhanifan <akmalyusufhanifan@gmail.com>
2026-04-30 14:51:51 +0700

    add few code: location

--- src/components/weather-dashboard/weather-dashboard.tsx
+++ src/components/weather-dashboard/weather-dashboard.tsx
@@ -5,6 +5,7 @@ import ForecastWeather from "./content/forecast-weather/forecast-weather";
 import ChanceOfRain from "@/components/weather-dashboard/content/chance-of-rain";
 import SearchBar from "@/components/weather-dashboard/content/search-bar";
 import { type WeatherData } from "@/types/weather-data";
+import { CiLocationOn } from "react-icons/ci";
 
 type Props = {
   data: WeatherData | null;
@@ -21,7 +22,15 @@ export default function WeatherDashboard({
 }: Props) {
   return (
     <div className="flex flex-col items-center">
-      <SearchBar onSearch={onSearch} />
+      <div className="flex justify-between items-center w-full">
+        <div className="flex items-center space-x-2">
+          <CiLocationOn className="text-xl text-[#FDFDFD]" />
+          <h2 className="text-xl text-[#FDFDFD]">
+            {data?.name}, {data?.sys.country}
+          </h2>
+        </div>
+        <SearchBar onSearch={onSearch} />
+      </div>
 
       <div className="flex items-center space-x-10 pt-10">
         <TodayWeather data={data} />
